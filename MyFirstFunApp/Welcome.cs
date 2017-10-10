c4c8d637e32c7c8f41520933b849f1a72013f0f1
Senthilrajan Kaliyaperumal <SKALI@statoil.com>
2017-10-10 19:20:22 +0530

    Add project files.

---
+++ MyFirstFunApp/Welcome.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace MyFirstFunApp
+{
+    public static class Welcome
+    {
+        [FunctionName("Welcome")]
+        public static async Task<object> Run([HttpTrigger(WebHookType = "genericJson")]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info($"Webhook was triggered!");
+
+            string jsonContent = await req.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject(jsonContent);
+
+            return req.CreateResponse(HttpStatusCode.OK, new
+            {
+                greeting = $"Welcome to Azure Funtion apps"
+            });
+        }
+    }
+}
